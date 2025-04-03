b8d0b6d50e043f4f92efd841d8ad831b21f2fffd
daniel-pedrozo <daniel.pedrozo8266@gmail.com>
2025-04-03 15:23:06 -0300

    pref: add some this like the way the code flows to run better and a clean code. feat: add some new functions to comunicate with the server side

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 import requests
 
-
 url = "http://127.0.0.1:5000"
-
 
 def check_connection():
     response = requests.get(f"{url}/connection")
@@ -16,14 +14,14 @@ def request_int():
     response = requests.get(f"{url}/get-inteiro")
     print(response.text if response.status_code == 200 else "Integer request failed")
 
-def request_even():
-    response = requests.get(f"{url}/get-even")
-    print(response.text if response.status_code == 200 else "Even number request failed")
+def request_even(client_id):
+    response = requests.post(f"{url}/get-even", json={"client_id": client_id})
+    print(response.json() if response.status_code == 200 else "Even number request failed")
 
-def request_odd():
-    response = requests.get(f"{url}/get-odd")
-    print(response.text if response.status_code == 200 else "Odd number request failed")
-    
+def request_odd(client_id):
+    response = requests.post(f"{url}/get-odd", json={"client_id": client_id})
+    print(response.json() if response.status_code == 200 else "Odd number request failed")
+
 def pass_ident(client_id):
     response = requests.post(f"{url}/pass-ident", json={"client_id": client_id})
     print(response.json() if response.status_code == 200 else "Identification failed")
@@ -34,14 +32,13 @@ def get_last_number(client_id):
 
 def main():
     check_connection()
-    request_word()
-    check_connection()
-    request_int()
 
-    client_id = input("What is the client id?")
+    client_id = input("Enter client ID: ")
     pass_ident(client_id)
-    get_last_number(client_id)
 
+    get_last_number(client_id)
+    #request_even(client_id)
+    request_odd(client_id)
 
 if __name__ == "__main__":
     main()
